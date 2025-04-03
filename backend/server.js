af1698071a4853215f803e2bcf67b297137f3b54
Narek Tamazyan <natamazy@gmail.com>
2025-04-03 19:42:56 +0400

    backend | fix | using jwt token from env

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,15 @@
 import Fastify from 'fastify';
 import jwt from '@fastify/jwt';
 import formbody from '@fastify/formbody';
+import 'dotenv/config';
 
 import authRoutes from './routes/auth.js';
-
 
 const fastify = Fastify({ logger: true });
 
 fastify.register(formbody);
 fastify.register(jwt, {
-  secret: process.env.JWT_SECRET || 'supersecretkey',
+  secret: process.env.JWT_SECRET,
 });
 
 fastify.register(authRoutes, { prefix: '/auth' });
