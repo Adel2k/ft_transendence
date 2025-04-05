a905d6a895b05e922610bbb6beb379e39e99e567
Narek Tamazyan <natamazy@gmail.com>
2025-04-05 14:39:53 +0400

    backend | dev | cleared server.js, made modular

---
+++ backend/routes/index.js
@@ -0,0 +1,7 @@
+import authRoutes from './auth.js';
+import userRoutes from './user.js';
+
+export default async function registerRoutes(fastify, opts) {
+    fastify.register(authRoutes, { prefix: '/auth' });
+    fastify.register(userRoutes, { prefix: '/user' });
+}
--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,15 @@
 import Fastify from 'fastify';
-import jwt from '@fastify/jwt';
 import formbody from '@fastify/formbody';
 import 'dotenv/config';
 
-import userRoutes from './routes/user.js';
-import authRoutes from './routes/auth.js';
+import jwtPlugin from './plugins/jwt.js';
+import registerRoutes from './routes/index.js';
 
 const fastify = Fastify({ logger: true });
 
 fastify.register(formbody);
-fastify.register(jwt, {
-	secret: process.env.JWT_SECRET,
-});
-
-fastify.decorate("authenticate", async function (request, reply) {
-	try {
-		await request.jwtVerify();
-	} catch (err) {
-		reply.send(err);
-	}
-});
-
-fastify.register(authRoutes, { prefix: '/auth' });
-fastify.register(userRoutes, { prefix: '/user' });
+fastify.register(jwtPlugin);
+fastify.register(registerRoutes);
 
 fastify.listen({ port: 3000 }, (err) => {
 	if (err) throw err;
